8f8eb036cb06d5578b13dc87ecceaf43150a2a37
Purwa Wiadnyana <purwawiadnyana08@gmail.com>
2024-11-07 17:35:57 +0800

    commit setup

---
+++ src/main/java/com/purwa/TechnicalTestAP/TechnicalTestApApplication.java
@@ -0,0 +1,13 @@
+package com.purwa.TechnicalTestAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purwa/TechnicalTestAP/TechnicalTestApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purwa.TechnicalTestAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
